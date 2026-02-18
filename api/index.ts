f59d61c11753a2dc3eb7a76cc25d2972f4583bef
ulyana <novikova.ula.007@gmail.com>
2026-02-18 05:57:39 +0300

    [api] add POST requests encode, decode

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,42 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
 const app = express();
 const port = 8000;
 
+const Vigenere = require('caesar-salad').Vigenere;
+
 app.use(cors());
+app.use(express.json())
+
+app.post('/encode', (req: Request, res: Response) => {
+    if (!req.body.password || !req.body.message) {
+        return res.status(400).send({error: "Please enter a message and a password"});
+    } else {
+        const vigenereObject = {
+            message: req.body.message,
+            password: req.body.password
+        }
+
+        const encodeText = Vigenere.Cipher(vigenereObject.password).crypt(vigenereObject.message);
+        return res.send({"encoded": encodeText});
+    }
+
+})
+
+app.post('/decode', (req: Request, res: Response) => {
+    if (!req.body.password || !req.body.message) {
+        return res.status(400).send({error: "Please enter a message and a password"});
+    } else {
+        const vigenereObject = {
+            message: req.body.message,
+            password: req.body.password
+        }
+
+        const decodeText = Vigenere.Decipher(vigenereObject.password).crypt(vigenereObject.message);
+        return res.send({"decoded": decodeText});
+    }
+
+})
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
