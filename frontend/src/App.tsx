54fed8ae0df53dbfedb9f905353b455ed0cc8db7
ulyana <novikova.ula.007@gmail.com>
2026-02-18 06:25:34 +0300

    [frontend] vigenere form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import {Container} from "@mui/material";
+import Vigenere from "./features/vigenere/Vigenere.tsx";
 
 const App = () => {
-  return (
-      <Container maxWidth='lg'>
-
-      </Container>
-  )
+    return (
+        <Container maxWidth='lg'>
+            <Vigenere/>
+        </Container>
+    )
 }
 
 export default App
--- frontend/src/features/vigenere/Vigenere.tsx
+++ frontend/src/features/vigenere/Vigenere.tsx
@@ -1,6 +1,56 @@
+import {Box, IconButton, Stack, TextField} from "@mui/material";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
+import {useState} from "react";
+
 const Vigenere = () => {
+    const [decoded, setDecoded] = useState('');
+    const [password, setPassword] = useState('');
+    const [encoded, setEncoded] = useState('');
+
     return (
-        <></>
+        <Box sx={{maxWidth: '50%', p: 4}}>
+            <form>
+                <Stack spacing={2}>
+                    <TextField
+                        label="Decoded message"
+                        multiline
+                        rows={6}
+                        fullWidth
+                        value={decoded}
+                        onChange={(e) => setDecoded(e.target.value)}
+                    />
+
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Stack>
+                            <IconButton disabled={!(password.trim().length > 0)}>
+                                <ArrowDownwardIcon/>
+                            </IconButton>
+                            <IconButton disabled={!(password.trim().length > 0)}>
+                                <ArrowUpwardIcon/>
+                            </IconButton>
+                        </Stack>
+                    </Stack>
+
+                    <TextField
+                        label="Encoded message"
+                        rows={6}
+                        multiline
+                        fullWidth
+                        value={encoded}
+                        onChange={(e) => setEncoded(e.target.value)}
+                    />
+                </Stack>
+            </form>
+        </Box>
     );
 };
 
